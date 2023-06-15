cacbf2489183914ac0cb3363dde447fd88be8444
Eduardo Guzmán H <eduardo.guzman@evertecinc.com>
2023-06-15 15:29:45 -0500

    Updates logo

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -19,7 +19,7 @@ export function Layout({ children, sections = [] }) {
           <div className="contents lg:pointer-events-auto lg:block lg:w-72 lg:overflow-y-auto lg:border-r lg:border-gray-900/10 lg:px-6 lg:pb-8 lg:pt-4 lg:dark:border-white/10 xl:w-80">
             <div className="hidden lg:flex">
               <Link href="/" aria-label="Home">
-                <Logo className="h-6" />
+                <Logo className="h-8" />
               </Link>
             </div>
             <Header />
