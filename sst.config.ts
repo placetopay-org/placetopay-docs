cfa7dee66fceee92dcc07ea32bd198dfbe63cab2
Eduardo Guzmán H <eduardo.guzman@evertecinc.com>
2023-08-01 13:20:27 -0500

    Deploy

--- sst.config.ts
+++ sst.config.ts
@@ -26,6 +26,10 @@ export default {
       stack.addOutputs({
         SiteUrl: site.url,
       });
+
+      if (app.stage !== "prod") {
+        app.setDefaultRemovalPolicy("destroy");
+      }
     });
   },
 } satisfies SSTConfig;
