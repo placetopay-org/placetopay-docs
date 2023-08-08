5fd7751c8eac385ca7f62222bd3e0ef820f837b2
Meiyer Jaimes <meiyerjaimes@gmail.com>
2023-08-08 18:43:47 -0500

    feat: auto add openapi refs from recma

--- src/mdx/recma.mjs
+++ src/mdx/recma.mjs
@@ -1,5 +1,8 @@
 import { mdxAnnotations } from 'mdx-annotations'
 import recmaNextjsStaticProps from 'recma-nextjs-static-props'
+import esprima from 'esprima';
+import SwaggerParser from '@apidevtools/swagger-parser'
+import path from 'path'
 
 function recmaRemoveNamedExports() {
   return (tree) => {
@@ -12,8 +15,50 @@ function recmaRemoveNamedExports() {
   }
 }
 
+function recmaAddOpenapiRefsToStaticProps() {
+  return async (tree) => {
+    const filePath = path.join(process.cwd(), 'public', 'api-ES.yaml')
+    const api = await SwaggerParser.dereference(filePath);
+    let apiRefs = null;
+
+    tree.body = tree.body.map((node) => {
+      if (node.type === "VariableDeclaration" && node.declarations[0].id.name === 'apiRefs') {
+        const refs = node.declarations[0].init.elements
+        const apiRefsWithOpenapi = refs.reduce((acc, apiRef) => {
+          const apiRefValue = apiRef.value
+          return { ...acc, [apiRefValue]: api.paths[apiRefValue] }
+        }, {})
+
+        const ast = esprima.parseModule(`(${JSON.stringify(apiRefsWithOpenapi)})`, {
+          range: false,
+          loc: false,
+        });
+        apiRefs = ast.body[0].expression;
+      }
+      
+      if (node.type === 'ExportNamedDeclaration' && node.declaration.declarations[0].id.name === 'getStaticProps' && apiRefs !== null) {
+        node.declaration.declarations[0].init.body.properties[0].value.arguments[0].arguments[0].properties = [
+          ...node.declaration.declarations[0].init.body.properties[0].value.arguments[0].arguments[0].properties,
+          {
+            type: 'Property',
+            method: false,
+            shorthand: false,
+            computed: false,
+            kind: 'init',
+            key: { type: 'Identifier', name: 'refs' },
+            value: apiRefs,
+          }
+        ]
+      }
+
+      return node
+    })
+  }
+}
+
 export const recmaPlugins = [
   mdxAnnotations.recma,
   recmaRemoveNamedExports,
   recmaNextjsStaticProps,
+  recmaAddOpenapiRefsToStaticProps,
 ]
