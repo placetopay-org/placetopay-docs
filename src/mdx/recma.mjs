586a67e43df178ca746c45446e8f92634ac99c78
Meiyer Jaimes <meiyerjaimes@gmail.com>
2023-08-16 10:33:03 -0500

    feat: i18n support

--- src/mdx/recma.mjs
+++ src/mdx/recma.mjs
@@ -1,8 +1,5 @@
 import { mdxAnnotations } from 'mdx-annotations'
 import recmaNextjsStaticProps from 'recma-nextjs-static-props'
-import esprima from 'esprima';
-import SwaggerParser from '@apidevtools/swagger-parser'
-import path from 'path'
 
 function recmaRemoveNamedExports() {
   return (tree) => {
@@ -17,41 +14,72 @@ function recmaRemoveNamedExports() {
 
 function recmaAddOpenapiRefsToStaticProps() {
   return async (tree) => {
-    const filePath = path.join(process.cwd(), 'src', 'assets', 'apis', 'checkout', 'es.yaml')
-    const api = await SwaggerParser.dereference(filePath);
-    let apiRefs = null;
+    tree.body.splice(1, 0, {
+      type: 'ImportDeclaration',
+      specifiers: [
+        {
+          type: 'ImportSpecifier',
+          local: {
+            type: 'Identifier',
+            name: 'dereferenceOpenapi',
+          },
+          imported: {
+            type: 'Identifier',
+            name: 'dereferenceOpenapi',
+          },
+        },
+      ],
+      source: {
+        type: 'Literal',
+        value: '@/lib/dereferenceOpenapi',
+        raw: "'@/lib/dereferenceOpenapi'",
+      },
+    })
 
-    tree.body = tree.body.map((node) => {
-      if (node.type === "VariableDeclaration" && node.declarations[0].id.name === 'apiRefs') {
-        const refs = node.declarations[0].init.elements
-        const apiRefsWithOpenapi = refs.reduce((acc, apiRef) => {
-          const apiRefValue = apiRef.value
-          return { ...acc, [apiRefValue]: api.paths[apiRefValue] }
-        }, {})
-
-        const ast = esprima.parseModule(`(${JSON.stringify(apiRefsWithOpenapi)})`, {
-          range: false,
-          loc: false,
-        });
-        apiRefs = ast.body[0].expression;
-      }
-      
-      if (node.type === 'ExportNamedDeclaration' && node.declaration.declarations[0].id.name === 'getStaticProps' && apiRefs !== null) {
-        node.declaration.declarations[0].init.body.properties[0].value.arguments[0].arguments[0].properties = [
-          ...node.declaration.declarations[0].init.body.properties[0].value.arguments[0].arguments[0].properties,
-          {
-            type: 'Property',
-            method: false,
-            shorthand: false,
-            computed: false,
-            kind: 'init',
-            key: { type: 'Identifier', name: 'refs' },
-            value: apiRefs,
-          }
-        ]
-      }
-
-      return node
+    const getStaticPropsIndex = tree.body.findIndex((node) => node.type === 'ExportNamedDeclaration' && node.declaration.declarations[0].id.name === 'getStaticProps');
+    tree.body[getStaticPropsIndex].declaration.declarations[0].init.async = true;
+    tree.body[getStaticPropsIndex].declaration.declarations[0].init.body.properties[0].value.arguments[0].arguments[0].properties.push({
+      type: 'Property',
+      method: false,
+      shorthand: false,
+      computed: false,
+      kind: 'init',
+      key: { type: 'Identifier', name: 'refs' },
+      value: {
+        type: "AwaitExpression",
+        argument: {
+          type: "CallExpression",
+          callee: {
+            type: "Identifier",
+            name: "dereferenceOpenapi",
+          },
+          arguments: [
+            {
+              type: "MemberExpression",
+              computed: false,
+              object: {
+                type: "MetaProperty",
+                meta: {
+                  type: "Identifier",
+                  name: "import",
+                },
+                property: {
+                  type: "Identifier",
+                  name: "meta",
+                },
+              },
+              property: {
+                type: "Identifier",
+                name: "url",
+              },
+            },
+            {
+              type: "Identifier",
+              name: "apiRefs",
+            }
+          ],
+        },
+      },
     })
   }
 }
