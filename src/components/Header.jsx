58d6f54ed694c06ce4cbeccd8e01ada5577ddf3c
Meiyer Jaimes <meiyerjaimes@gmail.com>
2023-08-17 12:16:16 -0500

    feat: Language Switch design

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,6 +11,7 @@ import {
 import { useMobileNavigationStore } from '@/components/MobileNavigation'
 import { ModeToggle } from '@/components/ModeToggle'
 import { MobileSearch, Search } from '@/components/Search'
+import { LanguageSwitch } from './LanguageSwitch'
 
 function TopLevelNavItem({ href, children }) {
   return (
@@ -26,30 +27,20 @@ function TopLevelNavItem({ href, children }) {
 }
 
 export const HeaderHome = ({ className }) => {
-  let { isOpen: mobileNavIsOpen } = useMobileNavigationStore()
-  let isInsideMobileNavigation = useIsInsideMobileNavigation()
-
   return (
     <div
       className={clsx(
         className,
-        'flex items-center justify-between gap-12 px-4 sm:px-6 lg:px-8',
+        'flex items-center justify-between gap-12 px-4 sm:px-6 lg:px-8'
       )}
     >
-      <div
-        className={clsx(
-          'absolute inset-x-0 top-full h-px transition',
-          (isInsideMobileNavigation || !mobileNavIsOpen) &&
-            'bg-gray-900/7.5 dark:bg-white/7.5'
-        )}
-      />
       <Search />
       <div className="flex items-center gap-5 lg:hidden">
         <Link href="/" aria-label="Home">
           <Logo className="h-6" />
         </Link>
       </div>
-      <div className="flex items-center gap-5">
+      <div className="flex items-center gap-4">
         {/* <nav className="hidden md:block">
           <ul role="list" className="flex items-center gap-8">
           <TopLevelNavItem href="/">API</TopLevelNavItem>
@@ -57,6 +48,7 @@ export const HeaderHome = ({ className }) => {
               <TopLevelNavItem href="#">Support</TopLevelNavItem>
           </ul>
           </nav> */}
+        <LanguageSwitch />
         <div className="hidden md:block md:h-5 md:w-px md:bg-gray-900/10 md:dark:bg-white/15" />
         <div className="flex gap-4">
           <MobileSearch />
@@ -84,7 +76,8 @@ export const Header = forwardRef(function Header({ className }, ref) {
       className={clsx(
         className,
         'fixed inset-x-0 top-0 z-50 flex h-14 items-center justify-between gap-12 px-4 transition sm:px-6 lg:left-72 lg:z-30 lg:px-8 xl:left-80',
-        !isInsideMobileNavigation && 'backdrop-blur-sm dark:backdrop-blur lg:left-72 xl:left-80',
+        !isInsideMobileNavigation &&
+          'backdrop-blur-sm dark:backdrop-blur lg:left-72 xl:left-80',
         isInsideMobileNavigation
           ? 'bg-white dark:bg-gray-900'
           : 'bg-white/[var(--bg-opacity-light)] dark:bg-gray-900/[var(--bg-opacity-dark)]'
@@ -116,6 +109,7 @@ export const Header = forwardRef(function Header({ className }, ref) {
             <TopLevelNavItem href="#">Support</TopLevelNavItem> */}
         {/* </ul> */}
         {/* </nav> */}
+        <LanguageSwitch />
         <div className="hidden md:block md:h-5 md:w-px md:bg-gray-900/10 md:dark:bg-white/15" />
         <div className="flex gap-4">
           <MobileSearch />
