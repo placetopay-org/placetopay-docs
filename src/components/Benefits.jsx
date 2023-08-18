79573bcbe7b1c937dda45636e69d58119a9e7c27
Meiyer Jaimes <meiyerjaimes@gmail.com>
2023-08-18 16:17:38 -0500

    feat: index and footer i18n

--- src/components/Benefits.jsx
+++ src/components/Benefits.jsx
@@ -1,21 +1,25 @@
-import Link from 'next/link'
 import { motion, useMotionTemplate, useMotionValue } from 'framer-motion'
 
 import { BGPatternSmall } from '@/components/BGPatternSmall'
-import { Heading } from '@/components/Heading'
 import { BrushTwoToneIcon } from '@/components/iconsax/BrushTwoToneIcon'
 import { MonitorMobileTwoToneIcon } from '@/components/iconsax/MonitorMobileTwoToneIcon'
 import { MoneyReceiveTwoToneIcon } from '@/components/iconsax/MoneyReceiveTwoToneIcon'
 import { WalletTwoToneIcon } from '@/components/iconsax/WalletTwoToneIcon'
 import { SecuritySafeTwoToneIcon } from '@/components/iconsax/SecuritySafeTwoToneIcon'
 import { LampChargeTwoToneIcon } from '@/components/iconsax/LampChargeTwoToneIcon'
+import { useLocale } from './LocaleProvider'
 
 const resources = [
   {
     href: '/contacts',
-    name: 'Diseño personalizado',
-    description:
-      'Personaliza la apariencia de Checkout con logotipos y colores de tu empresa. Crea una experiencia de pago única y coherente con tu marca.',
+    name: {
+      es: 'Diseño personalizado',
+      en: 'Custom design',
+    },
+    description: {
+      es: 'Personaliza la apariencia de Checkout con logotipos y colores de tu empresa. Crea una experiencia de pago única y coherente con tu marca.',
+      en: 'Personalize the appearance of Checkout with your company logos and colors. Create a unique and consistent payment experience with your brand.',
+    },
     icon: BrushTwoToneIcon,
     pattern: {
       y: 16,
@@ -27,9 +31,14 @@ const resources = [
   },
   {
     href: '/conversations',
-    name: 'Múltiples idiomas y monedas',
-    description:
-      'Acepta pagos en diferentes idiomas y monedas para atraer a clientes de todo el mundo. Amplía tu alcance y aumenta tus oportunidades de venta.',
+    name: {
+      es: 'Múltiples idiomas y monedas',
+      en: 'Multiple languages and currencies',
+    },
+    description: {
+      es: 'Acepta pagos en diferentes idiomas y monedas para atraer a clientes de todo el mundo. Amplía tu alcance y aumenta tus oportunidades de venta.',
+      en: 'Accept payments in different languages and currencies to attract customers from all over the world. Expand your reach and increase your sales opportunities.',
+    },
     icon: MoneyReceiveTwoToneIcon,
     pattern: {
       y: -6,
@@ -41,9 +50,14 @@ const resources = [
   },
   {
     href: '/messages',
-    name: 'Optimizado para cualquier dispositivo',
-    description:
-      'Nuestro checkout está diseñado para adaptarse y funcionar sin problemas en dispositivos móviles y de escritorio. Brinda una experiencia de pago optimizada, independientemente del dispositivo que utilice tu cliente.',
+    name: {
+      es: 'Optimizado para cualquier dispositivo',
+      en: 'Optimized for any device',
+    },
+    description: {
+      es: 'Nuestro checkout está diseñado para adaptarse y funcionar sin problemas en dispositivos móviles y de escritorio. Brinda una experiencia de pago optimizada, independientemente del dispositivo que utilice tu cliente.',
+      en: 'Our checkout is designed to adapt and work seamlessly on mobile and desktop devices. Provide an optimized payment experience, regardless of the device your customer uses.',
+    },
     icon: MonitorMobileTwoToneIcon,
     pattern: {
       y: 32,
@@ -55,9 +69,11 @@ const resources = [
   },
   {
     href: '/groups',
-    name: 'Wallet PlacetoPay',
-    description:
-      'Con Checkout, tus clientes pueden aprovechar nuestra billetera digital, permitiéndoles guardar y gestionar sus medios de pago de forma segura. Esto agiliza el proceso de pago y brinda una mayor comodidad al realizar transacciones futuras.',
+    name: { es: 'Wallet PlacetoPay', en: 'PlacetoPay Wallet' },
+    description: {
+      es: 'Con Checkout, tus clientes pueden aprovechar nuestra billetera digital, permitiéndoles guardar y gestionar sus medios de pago de forma segura. Esto agiliza el proceso de pago y brinda una mayor comodidad al realizar transacciones futuras.',
+      en: 'With Checkout, your customers can take advantage of our digital wallet, allowing them to securely store and manage their payment methods. This streamlines the payment process and provides greater convenience when making future transactions.',
+    },
     icon: WalletTwoToneIcon,
     pattern: {
       y: 22,
@@ -66,9 +82,11 @@ const resources = [
   },
   {
     href: '/groups',
-    name: 'Seguridad de la información',
-    description:
-      'Nos tomamos en serio la protección de los datos sensibles. PlacetoPay Checkout cumple con los estándares de seguridad de la industria, incluidos los lineamientos PCI, para garantizar la seguridad en la captura y procesamiento de la información de tarjetas.',
+    name: { es: 'Seguridad de la información', en: 'Information security' },
+    description: {
+      es: 'Nos tomamos en serio la protección de los datos sensibles. PlacetoPay Checkout cumple con los estándares de seguridad de la industria, incluidos los lineamientos PCI, para garantizar la seguridad en la captura y procesamiento de la información de tarjetas.',
+      en: 'We take the protection of sensitive data seriously. PlacetoPay Checkout complies with industry security standards, including PCI guidelines, to ensure security in the capture and processing of card information.',
+    },
     icon: SecuritySafeTwoToneIcon,
     pattern: {
       y: 22,
@@ -77,9 +95,11 @@ const resources = [
   },
   {
     href: '/groups',
-    name: 'Pagos avanzados',
-    description:
-      'Nuestra plataforma ofrece una amplia gama de opciones de pago, incluyendo pagos con impuestos, pagos mixtos, suscripciones, cobros recurrentes y promociones. Ten el control total sobre tus transacciones y ofrece opciones flexibles a tus clientes.',
+    name: { es: 'Pagos avanzados', en: 'Advanced payments' },
+    description: {
+      es: 'Nuestra plataforma ofrece una amplia gama de opciones de pago, incluyendo pagos con impuestos, pagos mixtos, suscripciones, cobros recurrentes y promociones. Ten el control total sobre tus transacciones y ofrece opciones flexibles a tus clientes.',
+      en: 'Our platform offers a wide range of payment options, including tax payments, mixed payments, subscriptions, recurring charges, and promotions. Have full control over your transactions and offer flexible options to your customers.',
+    },
     icon: LampChargeTwoToneIcon,
     pattern: {
       y: 22,
@@ -103,7 +123,7 @@ function ResourcePattern({ mouseX, mouseY, ...gridProps }) {
   return (
     <div className="pointer-events-none">
       <div className="absolute inset-0 rounded-2xl transition duration-300 [mask-image:linear-gradient(white,transparent)] group-hover:opacity-50">
-        <BGPatternSmall className="group absolute inset-x-0 w-full fill-black/[0.04] stroke-black/[0.05] dark:fill-white/[0.04] dark:stroke-white/[0.05]"/>
+        <BGPatternSmall className="group absolute inset-x-0 w-full fill-black/[0.04] stroke-black/[0.05] dark:fill-white/[0.04] dark:stroke-white/[0.05]" />
       </div>
       <motion.div
         className="absolute inset-0 rounded-2xl bg-gradient-to-r from-cyan-100 to-teal-100 opacity-0 transition duration-300 group-hover:opacity-100 dark:from-sky-950 dark:to-teal-950"
@@ -113,7 +133,7 @@ function ResourcePattern({ mouseX, mouseY, ...gridProps }) {
         className="absolute inset-0 rounded-2xl opacity-0 mix-blend-overlay transition duration-300 group-hover:opacity-100"
         style={style}
       >
-        <BGPatternSmall className="absolute inset-x-0 w-full fill-black/50 stroke-black/70 dark:fill-white/[0.04] dark:stroke-white/10"/>
+        <BGPatternSmall className="absolute inset-x-0 w-full fill-black/50 stroke-black/70 dark:fill-white/[0.04] dark:stroke-white/10" />
       </motion.div>
     </div>
   )
@@ -154,11 +174,19 @@ function Resource({ resource }) {
 }
 
 export function Benefits() {
+  const { locale } = useLocale()
   return (
     <div className="xl:max-w-none">
       <div className="not-prose mt-4 grid grid-cols-1 gap-8  dark:border-white/5 sm:grid-cols-2 xl:grid-cols-3">
         {resources.map((resource) => (
-          <Resource key={resource.href} resource={resource} />
+          <Resource
+            key={resource.href}
+            resource={{
+              ...resource,
+              name: resource.name[locale],
+              description: resource.description[locale],
+            }}
+          />
         ))}
       </div>
     </div>
--- src/components/DocsList.jsx
+++ src/components/DocsList.jsx
@@ -1,37 +1,64 @@
-
 import { ItemList } from '@/components/ItemsList'
 import { useLocalizePath } from '@/hooks/useLocalizePath'
+import { useLocale } from './LocaleProvider'
 
 const guides = [
   {
     href: '/checkout',
-    action: 'Ver más',
-    name: 'Inicio',
-    description: 'Conoce Checkout, las ventajas que te ofrece y los métodos de integración a un alto nivel.',
+    action: { es: 'Ver más', en: 'Learn more' },
+    name: {
+      es: 'Inicio',
+      en: 'Home',
+    },
+    description: {
+      es: 'Conoce Checkout, las ventajas que te ofrece y los métodos de integración a un alto nivel.',
+      en: 'Get to know Checkout, the advantages it offers you and the integration methods at a high level.',
+    },
   },
   {
     href: '/checkout/authentication',
-    action: 'Ver más',
-    name: 'Integración',
-    description: 'Conoce las capacidades de Checkout, los conceptos y flujos que debes tener en cuenta para una integración exitosa.',
+    action: { es: 'Ver más', en: 'Learn more' },
+    name: {
+      es: 'Integración',
+      en: 'Integration',
+    },
+    description: {
+      es: 'Conoce las capacidades de Checkout, los conceptos y flujos que debes tener en cuenta para una integración exitosa.',
+      en: "Learn about Checkout's capabilities, the concepts and flows that you must take into account for a successful integration.",
+    },
   },
   {
     href: '/checkout/api-reference/session',
-    action: 'Ver más',
-    name: 'API',
-    description: 'Conoce en detalle los métodos HTTP disponibles, los parámetros y posibles respuestas de cada servicio.',
+    action: { es: 'Ver más', en: 'Learn more' },
+    name: {
+      es: 'API',
+      en: 'API',
+    },
+    description: {
+      es: 'Conoce en detalle los métodos HTTP disponibles, los parámetros y posibles respuestas de cada servicio.',
+      en: 'Know in detail the available HTTP methods, the parameters and possible responses of each service.',
+    },
   },
 ]
 
 export function DocsList() {
-  const localizePath = useLocalizePath();
+  const { locale } = useLocale()
+  const localizePath = useLocalizePath()
 
   return (
     <div className="my-8 xl:max-w-none">
       <div className="not-prose grid grid-cols-1 gap-8 border-gray-900/5 dark:border-white/5 sm:grid-cols-3">
-        {guides.map((guide) => ({...guide, href: localizePath(guide.href)})).map((guide) => (
-          <ItemList key={guide.name} {...guide}></ItemList>
-        ))}
+        {guides
+          .map((guide) => ({
+            ...guide,
+            href: localizePath(guide.href),
+            name: guide.name[locale],
+            description: guide.description[locale],
+            action: guide.action[locale],
+          }))
+          .map((guide) => (
+            <ItemList key={guide.name} {...guide}></ItemList>
+          ))}
       </div>
     </div>
   )
