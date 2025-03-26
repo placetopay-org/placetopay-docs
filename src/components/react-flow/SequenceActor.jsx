761ca1a5efeff5bbd01ad2b05e7719336667e084
Gabriela Muñoz Nupan <gabriela.munoz@evertecinc.com>
2025-03-26 17:27:27 -0500

    custom color for sequenceActor

--- src/components/react-flow/SequenceActor.jsx
+++ src/components/react-flow/SequenceActor.jsx
@@ -1,10 +1,9 @@
-import { Handle, Position } from 'reactflow';
-
-function SequenceActor({ data }) {
+function SequenceActor({ data, color = "bg-green-500" }) {
+  const barColor = data?.color || color;
   return (
   <div className="SequenceDiagramActorNode Box-root rounded-l" style={{height: '400px'}}>
     <div className="SequenceDiagramActorNode-heading Box-root w-40">
-    <div className="absolute left-0 top-0 h-6 w-[6px] bg-green-500 rounded-lg"></div>
+    <div className={`absolute left-0 top-0 h-6 w-[6px] ${barColor} rounded-lg`}></div>
     <div className="w-fit h-fit pl-4">
     <p className="font-bold text-gray-900 mt-0">{data.label ?? "Actor"}</p>
     </div>
