cc893461606c347d70e4406b3ad60a76bd6da9ea
Kevin Vargas <vargaskevin19@gmail.com>
2025-03-30 19:10:31 -0500

    fix text color

--- src/components/react-flow/SequenceActor.jsx
+++ src/components/react-flow/SequenceActor.jsx
@@ -4,7 +4,7 @@ function SequenceActor({ data, color = "bg-green-500" }) {
     <div>
       <div className="relative h-6 w-40 items-center flex">
         <div className={`h-6 w-[6px] ${barColor} rounded-lg`}></div>
-        <p className="font-bold text-gray-900 m-0 mt-0 ml-4">{data.label ?? "Actor"}</p>
+        <p className="font-bold text-gray-900 dark:text-white m-0 mt-0 ml-4">{data.label ?? "Actor"}</p>
       </div>
       <div className="relative bg-gray-300 left-[2px] w-[2px] rounded" style={{ height: data.height }}></div>
     </div>
