71656fa3bb80784d0f97ed4489d28834e629aa2c
Kevin Vargas <vargaskevin19@gmail.com>
2025-03-30 19:06:17 -0500

    wip

--- src/components/react-flow/DiagramSequence.jsx
+++ src/components/react-flow/DiagramSequence.jsx
@@ -5,162 +5,126 @@ import {
   Controls,
   MarkerType,
 } from 'reactflow';
- 
-import {nodeTypes} from "@/components/react-flow/react-flow";
 
-function generateFlowId(type, ...parts) {
-  return `${type}-${parts.join('-')}-${Date.now()}-${Math.random().toString(36).substr(2, 5)}`;
-}
+import { nodeTypes } from "@/components/react-flow/react-flow";
 
-function DiagramSequence({children}) {
+function DiagramSequence({ children }) {
   const data = {};
+
   const initialEdges = [];
 
-  const initialNodes = children
-  .reduce((acc, child) => {
-    if (React.isValidElement(child)) {
-      if (child.type.name === "SequenceActor") {
-        
-        data[child.props.name] = {
-          positionX: Number(child.props.positionX),
-          positionY: Number(child.props.positionY),
-          color: child.props.color,
-          name: child.props.name
-        };
+  const nodes = [];
+  const edges = [];
 
-        acc.push({
-          id: child.props.name,
-          type: 'actor',
-          position: { x: Number(child.props.positionX), y: Number(child.props.positionY) },
-          data: { 
-            label: child.props.label,
-            color: child.props.color,
-            positionX: child.props.positionX,
-            positionY: child.props.positionY
-          },
-        });
-      }
 
-      if (child.type.name === "SequenceAction") {
-        const idsNode = {
-          [child.props.from]: {
-            id: `node' + ${child.props.from} - ${Date.now()}-${Math.random().toString(36).substr(2, 5)}`,
-            positionX: data[child.props.from].positionX,
-            positionY: Number(child.props.positionY) - 11
-          },
-          [child.props.to]: {
-            id: `node' + ${child.props.to} - ${Date.now()}-${Math.random().toString(36).substr(2, 5)}`,
-            positionX: data[child.props.to].positionX,
-            positionY: Number(child.props.positionY) - 11
-          } ,
-        };
+  const actorsData = {};
 
-        const newNodes = [];
-
-        const fromNodeExists = acc.some(existingNode => 
-          existingNode.type === 'nodeAction' &&
-          existingNode.data?.from === child.props.from &&
-          existingNode.position.x === idsNode[child.props.from].positionX &&
-          existingNode.position.y === idsNode[child.props.from].positionY
-        );
-
-        if (!fromNodeExists) {
-          newNodes.push({
-            id: idsNode[child.props.from].id,
-            type: 'nodeAction',
-            position: { 
-              x: idsNode[child.props.from].positionX, 
-              y: idsNode[child.props.from].positionY
-            },
-            data: {
-              label: child.props.label,
-              from: child.props.from,
-              to: child.props.to,
-              color: data[child.props.from].color,
-              isBack: child.props.isBack,
-            }
-          });
-        }
-
-        const toNodeExists = acc.some(existingNode => 
-          existingNode.type === 'nodeAction' &&
-          existingNode.data?.to === child.props.to &&
-          existingNode.position.x === idsNode[child.props.to].positionX &&
-          existingNode.position.y === idsNode[child.props.to].positionY
-        );
-
-        if (!toNodeExists) {
-          newNodes.push({
-            id: idsNode[child.props.to].id,
-            type: 'nodeAction',
-            position: { 
-              x: idsNode[child.props.to].positionX, 
-              y: idsNode[child.props.to].positionY
-            },
-            data: {
-              label: child.props.label,
-              from: child.props.from,
-              to: child.props.to,
-              color: data[child.props.to].color,
-              isBack: child.props.isBack,
-            }
-          });
-        }
-
-        newNodes.push({
-          id: child.props.name,
-          type: 'action',
-          position: { 
-            x: Number(child.props.positionX), 
-            y: Number(child.props.positionY) 
-          },
-          data: { 
-            name: child.props.name,
-            label: child.props.message,
-            rounded: true,
-            isBack: child.props.isBack,
-          }
-        });
-      
-        acc.push(...newNodes);
-
-        initialEdges.push({
-          id: `edge-${child.props.from}-${Date.now()}-${Math.random().toString(36).substr(2, 5)}`,
-          source: idsNode[child.props.from].id,
-          target: child.props.name,
-          type: 'default',
-          markerEnd: {
-            type: MarkerType.ArrowClosed,
-          }
-        },
-        {
-          id: `edge-${child.props.to}-${Date.now()}-${Math.random().toString(36).substr(2, 5)}` ,
-          source: child.props.name,
-          target: idsNode[child.props.to].id,
-          type: 'default',
-          markerEnd: {
-            type: MarkerType.ArrowClosed,
-          }
-        });
-      }
+  for (const child of children) {
+    if (!React.isValidElement(child)) {
+      continue;
     }
-    return acc;
-  }, []);
+
+    let nodeData = {
+      position: { x: Number(child.props.positionX), y: Number(child.props.positionY) },
+      data: {
+        label: child.props.label,
+        color: child.props.color,
+      }
+    };
+
+    if (child.type.name === "SequenceActor") {
+
+      nodeData['id'] = 'actor_' + child.props.name;
+      nodeData['type'] = 'actor';
+      nodeData['data']['height'] = child.props.height;
+      actorsData[child.props.name] = { position: nodeData.position, color: child.props.color };
+
+    } else if (child.type.name === "SequenceAction") {
+
+      nodeData['id'] = 'action_' + child.props.name;
+      nodeData['type'] = 'action';
+      nodeData['data']['label'] = child.props.message;
+      nodeData['data']['isBack'] = child.props.isBack;
+      nodeData['data']['selfAction'] = child.props.selfAction;
+
+      let sourceActionPointPositionY = child.props.selfAction ? Number(child.props.sourcePositionY) : nodeData.position.y;
+      let sourceActionPoint = nodes.find(node => node.data.actor === child.props.from && node.position.y === sourceActionPointPositionY);
+      if (sourceActionPoint === undefined) {
+        sourceActionPoint = {
+          id: 'action_point_' + nodeData['id'] + '_' + child.props.from + Math.random().toString(36),
+          type: 'actionPoint',
+          position: {
+            x: actorsData[child.props.from].position.x,
+            y: sourceActionPointPositionY
+          },
+          data: {
+            color: actorsData[child.props.from].color,
+            isBack: child.props.isBack,
+            actor: child.props.from,
+          }
+        };
+      }
+      nodes.push(sourceActionPoint);
+
+      let targetActionPointPositionY = child.props.selfAction ? Number(child.props.targetPositionY) : nodeData.position.y;
+      let targetActionPoint = nodes.find(node => node.data.actor === child.props.to && node.position.y === targetActionPointPositionY);
+      if (targetActionPoint === undefined) {
+        targetActionPoint = {
+          id: 'action_point_' + nodeData['id'] + '_' + child.props.to + '_' + Math.random().toString(36),
+          type: 'actionPoint',
+          position: {
+            x: actorsData[child.props.to].position.x,
+            y: targetActionPointPositionY
+          },
+          data: {
+            color: actorsData[child.props.to].color,
+            isBack: child.props.isBack,
+            actor: child.props.to,
+          }
+        };
+      }
+      nodes.push(targetActionPoint);
+
+      edges.push( {
+        id: `edge_${nodeData['id']}_${child.props.from}_${Math.random().toString(36)}`,
+        source: sourceActionPoint.id,
+        sourceHandle:  Boolean(child.props.isBack) && !Boolean(sourceActionPoint.data.isBack) ? 'source_self_action' : 'default_source',
+        target: nodeData['id'],
+        targetHandle: 'default_target',
+        type: 'default',
+        markerEnd: {
+          type: MarkerType.ArrowClosed,
+        }
+      },  {
+        id: `edge_${nodeData['id']}_${child.props.to}_${Math.random().toString(36)}`,
+        source: nodeData['id'],
+        sourceHandle: 'default_source',
+        target: targetActionPoint.id,
+        targetHandle: Boolean(child.props.selfAction) ? 'target_self_action' : 'default_target',
+        type: 'default',
+        markerEnd: {
+          type: MarkerType.ArrowClosed,
+        }
+      });
+    }
+
+    nodes.push(nodeData);
+  }
 
   return (
     <div style={{ width: '100%', height: '300px' }} className="overflow-hidden ring-1 ring-gray-900/7.5 dark:ring-white/10 rounded-2xl">
-        <ReactFlow
-        nodes={initialNodes}
-        edges={initialEdges}
+      <ReactFlow
+        nodes={nodes}
+        edges={edges}
         nodeTypes={nodeTypes}
         fitView
         className="bg-gray-50 dark:bg-gray-800">
         <Background variant="dots" gap={12} size={1} />
-        <Controls showInteractive={false}/>
-        </ReactFlow>
+        <Controls showInteractive={false} />
+      </ReactFlow>
     </div>
 
   );
 }
- 
+
 export default DiagramSequence;
--- src/components/react-flow/react-flow.jsx
+++ src/components/react-flow/react-flow.jsx
@@ -1,93 +1,60 @@
-import {Tag} from "@/components/Tag";
-import {Handle, Position} from "reactflow";
+import { Tag } from "@/components/Tag";
+import { Handle, Position } from "reactflow";
 import clsx from "clsx";
 
-export function TagNode({data}) {
-    const targetHandle = data.targetHandle ?? true;
-    const sourceHandle = data.sourceHandle ?? true;
-    const targetPosition = data.targetPosition ?? Position.Left;
-    const sourcePosition = data.sourcePosition ?? Position.Right;
+import SequenceActor from "@/components/react-flow/SequenceActor";
+import SequenceAction from "@/components/react-flow/SequenceAction";
 
-    return (
-        <div className="bg-white py-2 px-3 border rounded-lg">
-            <div className="flex flex-col items-center space-y-1">
-                <span className="font-medium text-sm">{data.label}</span>
-                <Tag color={data.tagColor}>{data.tagLabel}</Tag>
-            </div>
-            {targetHandle && <Handle type="target" position={targetPosition} className="w-16 !bg-primary-500"/>}
-            {sourceHandle && <Handle type="source" position={sourcePosition} className="w-16 !bg-primary-500"/>}
-        </div>
-    );
+export function TagNode({ data }) {
+  const targetHandle = data.targetHandle ?? true;
+  const sourceHandle = data.sourceHandle ?? true;
+  const targetPosition = data.targetPosition ?? Position.Left;
+  const sourcePosition = data.sourcePosition ?? Position.Right;
+
+  return (
+    <div className="bg-white py-2 px-3 border rounded-lg">
+      <div className="flex flex-col items-center space-y-1">
+        <span className="font-medium text-sm">{data.label}</span>
+        <Tag color={data.tagColor}>{data.tagLabel}</Tag>
+      </div>
+      {targetHandle && <Handle type="target" position={targetPosition} className="w-16 !bg-primary-500" />}
+      {sourceHandle && <Handle type="source" position={sourcePosition} className="w-16 !bg-primary-500" />}
+    </div>
+  );
 }
 
-export function CustomNode({data}) {
-    const targetHandle = data.targetHandle ?? true;
-    const sourceHandle = data.sourceHandle ?? true;
-    const targetPosition = data.isBack 
-    ? Position.Right 
-    : data.targetPosition ?? Position.Left;
-  
-  const sourcePosition = data.isBack 
-    ? Position.Left 
+export function CustomAction({ data }) {
+  const targetHandle = data.targetHandle ?? true;
+  const sourceHandle = data.sourceHandle ?? true;
+
+  let targetPosition = data.isBack ? Position.Right : data.targetPosition ?? Position.Left;
+  let sourcePosition = data.isBack
+    ? Position.Left
     : data.sourcePosition ?? Position.Right;
 
-    return (
-        <div
-            className={clsx("px-3 py-2 shadow-sm bg-white border border-gray-200", data.rounded ? 'rounded-full' : 'rounded-md')}>
-            <div className="flex">
-                <div 
-                className="text-gray-700 text-sm" 
-                dangerouslySetInnerHTML={{ __html:data.label }} />
-            </div>
+  if(Boolean(data.selfAction)){
+    targetPosition = Position.Top;
+    sourcePosition = Position.Bottom;
+  }
 
-            {targetHandle && <Handle type="target" position={targetPosition} className="w-16 !bg-primary-500"/>}
-            {sourceHandle && <Handle type="source" position={sourcePosition} className="w-16 !bg-primary-500"/>}
-        </div>
-    );
+  return (
+    <div
+      className={clsx("px-3 py-2 shadow-sm bg-white border border-gray-200", data.rounded ? 'rounded-full' : 'rounded-md')}>
+      <div className="flex">
+        <div
+          className="text-gray-700 text-sm"
+          dangerouslySetInnerHTML={{ __html: data.label }} />
+      </div>
+
+      {targetHandle && <Handle type="target" position={targetPosition} className=" !bg-primary-500" />}
+      {sourceHandle && <Handle type="source" position={sourcePosition} className=" !bg-primary-500" />}
+    </div>
+  );
 }
 
-function SequenceActor({ data, color = "bg-green-500" }) {
-    const barColor = data?.color || color;
-    return (
-    <div className="SequenceDiagramActorNode Box-root rounded-l" style={{height: '400px'}}>
-      <div className="SequenceDiagramActorNode-heading Box-root w-40">
-      <div className={`absolute left-0 top-0 h-6 w-[6px] ${barColor} rounded-lg`}></div>
-      <div className="w-fit h-fit pl-4">
-      <p className="font-bold text-gray-900 mt-0">{data.label ?? "Actor"}</p>
-      </div>
-      </div>
-    </div>
-  
-    );
-  }
-
-  function SequenceAction({ data, color = "bg-green-500" }) {
-    const barColor = data?.color || color;
-
-    const targetPosition = data.isBack ? Position.Right : Position.Left;
-    const sourcePosition = data.isBack ? Position.Left : Position.Right;
-
-    return (
-        <div className="relative h-16 w-[6px]">
-          <div className={`absolute inset-0 ${barColor} rounded-lg`}>
-          <Handle 
-            type="target" 
-            position={targetPosition} 
-            className="!w-3 !h-3 !bg-primary-500"
-          />
-          <Handle 
-            type="source" 
-            position={sourcePosition} 
-            className="!w-3 !h-3 !bg-primary-500"
-          />
-          </div>
-        </div>
-      );
-  }
-
 export const nodeTypes = {
-    action: CustomNode,
-    tag: TagNode,
-    actor: SequenceActor,
-    nodeAction: SequenceAction
+  action: CustomAction,
+  tag: TagNode,
+  actor: SequenceActor,
+  actionPoint: SequenceAction
 };
