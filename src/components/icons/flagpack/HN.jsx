264db8b0b64f8fa0871a3bf48488ebb0b4100b01
Ivan Andres L <andresgom11012@gmail.com>
2025-04-01 18:03:25 -0500

    feat: adds flagpack icons

---
+++ src/components/icons/flagpack/HN.jsx
@@ -0,0 +1,63 @@
+export function HN(props) {
+    return (
+        <svg
+            {...props}
+            xmlns="http://www.w3.org/2000/svg"
+            width="27"
+            height="21"
+            fill="none"
+            viewBox="0 0 27 21"
+        >
+            <g clipPath="url(#clip0_2748_6865)">
+                <mask
+                    id="mask0_2748_6865"
+                    width="27"
+                    height="21"
+                    x="0"
+                    y="0"
+                    maskUnits="userSpaceOnUse"
+                    style={{ maskType: "luminance" }}
+                >
+                    <path fill="#fff" d="M0 .738h26.667v20H0z"></path>
+                </mask>
+                <g mask="url(#mask0_2748_6865)">
+                    <path
+                        fill="#F7FCFF"
+                        fillRule="evenodd"
+                        d="M0 .738v20h26.667v-20z"
+                        clipRule="evenodd"
+                    ></path>
+                    <mask
+                        id="mask1_2748_6865"
+                        width="27"
+                        height="21"
+                        x="0"
+                        y="0"
+                        maskUnits="userSpaceOnUse"
+                        style={{ maskType: "luminance" }}
+                    >
+                        <path
+                            fill="#fff"
+                            fillRule="evenodd"
+                            d="M0 .738v20h26.667v-20z"
+                            clipRule="evenodd"
+                        ></path>
+                    </mask>
+                    <g
+                        fill="#4564F9"
+                        fillRule="evenodd"
+                        clipRule="evenodd"
+                        mask="url(#mask1_2748_6865)"
+                    >
+                        <path d="M0 .738v6.666h26.667V.738zM0 14.07v6.668h26.667V14.07zM14.015 11.68l-.98.516.188-1.091-.793-.852h1.095l.49-1.072.49 1.072H15.6l-.792.852.187 1.09zM9.015 10.014l-.98.515.187-1.09-.792-.853h1.095l.49-1.072.49 1.072H10.6l-.792.852.187 1.091zM9.015 13.348l-.98.515.187-1.091-.792-.853h1.095l.49-1.072.49 1.072H10.6l-.792.853.187 1.09zM19.015 10.014l-.98.515.188-1.09-.793-.853h1.095l.49-1.072.49 1.072H20.6l-.792.852.187 1.091zM19.015 13.348l-.98.515.188-1.091-.793-.853h1.095l.49-1.072.49 1.072H20.6l-.792.853.187 1.09z"></path>
+                    </g>
+                </g>
+            </g>
+            <defs>
+                <clipPath id="clip0_2748_6865">
+                    <rect width="26.667" height="20" y="0.738" fill="#fff" rx="3"></rect>
+                </clipPath>
+            </defs>
+        </svg>
+    )
+}
---
+++ src/components/icons/flagpack/index.js
@@ -0,0 +1,8 @@
+export { default as PR } from './PR';
+export { default as US } from './US';
+export { default as EC } from './EC';
+export { default as CL } from './CL';
+export { default as MX } from './MX';
+export { default as HN } from './HN';
+export { default as CR } from './CR';
+export { default as CO } from './CO';
