23658bf216991fa12d3a21b00f255a91a68d59f5
Ivan Andres L <andresgom11012@gmail.com>
2025-04-01 18:03:25 -0500

    feat: adds flagpack icons

---
+++ src/components/icons/flagpack/CL.jsx
@@ -0,0 +1,47 @@
+export function CL(props) {
+    return (
+        <svg
+            {...props}
+            xmlns="http://www.w3.org/2000/svg"
+            width="28"
+            height="21"
+            fill="none"
+            viewBox="0 0 28 21"
+        >
+            <g clipPath="url(#clip0_2748_7010)">
+                <mask
+                    id="mask0_2748_7010"
+                    width="28"
+                    height="21"
+                    x="0"
+                    y="0"
+                    maskUnits="userSpaceOnUse"
+                    style={{ maskType: "luminance" }}
+                >
+                    <path fill="#fff" d="M.667.738h26.667v20H.667z"></path>
+                </mask>
+                <g fillRule="evenodd" clipRule="evenodd" mask="url(#mask0_2748_7010)">
+                    <path fill="#3D58DB" d="M.667.738h11.666v11.666H.667z"></path>
+                    <path fill="#F7FCFF" d="M12.333-.93h15v13.334h-15z"></path>
+                    <path fill="#E31D1C" d="M.667 12.404h26.666v8.334H.667z"></path>
+                    <path
+                        fill="#F7FCFF"
+                        d="m6.512 8.89-3.17 1.893 1.564-3.22-2.77-2.092 3.198-.035L6.541 2.58l.776 2.856 3.027.015-2.302 2.046 1.195 3.286z"
+                    ></path>
+                </g>
+            </g>
+            <defs>
+                <clipPath id="clip0_2748_7010">
+                    <rect
+                        width="26.667"
+                        height="20"
+                        x="0.667"
+                        y="0.738"
+                        fill="#fff"
+                        rx="3"
+                    ></rect>
+                </clipPath>
+            </defs>
+        </svg>
+    )
+}
