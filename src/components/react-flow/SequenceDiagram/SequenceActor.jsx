7c27002f108e01e48922649d1144fecbc97a4e1f
Gabriela Muñoz Nupan <gabriela.munoz@evertecinc.com>
2025-04-21 08:50:11 -0500

    fix: adjust style of sequenceActor on dark mode

--- src/components/react-flow/SequenceDiagram/SequenceActor.jsx
+++ src/components/react-flow/SequenceDiagram/SequenceActor.jsx
@@ -1,7 +1,7 @@
 function SequenceActor({ data }) {
   return (
     <div>
-        <p className="relative -left-[55%] font-bold text-gray-900 dark:text-white shadow-sm bg-white border border-gray-200 m-0 mt-0 ml-4 p-1 rounded-lg">{data.label ?? "Actor"}</p>
+      <p className="relative -left-[55%] font-bold text-gray-900 shadow-sm bg-white border border-gray-200 m-0 mt-0 ml-4 p-1 rounded-lg">{data.label ?? "Actor"}</p>
       <div className="relative bg-gray-300 left-[2px] w-[2px] rounded" style={{ height: data.height }}></div>
     </div>
   );
