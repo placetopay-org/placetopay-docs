b405bcf614c9583c5cd04b7607d65c64facf290a
JuanM17 <126290147+juanmunoz16@users.noreply.github.com>
2025-09-09 11:16:33 -0500

    feat: update images

--- src/components/Benefitsnewpanel.jsx
+++ src/components/Benefitsnewpanel.jsx
@@ -1,11 +1,7 @@
-import { DocumentTextTwoToneIcon } from '@/components/iconsax/DocumentTextTwoToneIcon'
-import { PeronalizationTwoToneIcon } from '@/components/iconsax/PeronalizationTwoToneIcon'
-import { ImportTwoToneIcon } from '@/components/iconsax/ImportTwoToneIcon'
-import { TransactionTwoToneIcon } from '@/components/iconsax/TransactionTwoToneIcon'
-import { FlexibilityTwoToneIcon } from '@/components/iconsax/FlexibilityTwoToneIcon'
-import { ErrorTwoToneIcon } from '@/components/iconsax/ErrorTwoToneIcon'
+
 import { useLocale } from './LocaleProvider'
 import { ResourceCard } from './ResourceCard'
+import { DocumentText, Activity, ReceiptSearch, BackSquare, UserSearch, SecuritySafe} from 'iconsax-react'
 
 const resources = [
   {
@@ -17,7 +13,7 @@ const resources = [
       es: 'Consulta el estado de cada transacción con filtros por fecha, monto, medio de pago, referencia, entre otros.',
       en: 'View the status of each transaction with filters by date, amount, payment method, reference, and more.',
     },
-    icon: DocumentTextTwoToneIcon,
+    icon: () => <Activity variant="TwoTone"/>,
     pattern: {
       y: 16,
       squares: [
@@ -35,7 +31,7 @@ const resources = [
       es: 'Registra los eventos realizados en el sistema para fines de auditoría y control.',
       en: 'Track key events for audit and control purposes.',
     },
-    icon: ImportTwoToneIcon,
+    icon: () => <ReceiptSearch variant="TwoTone"/>,
     pattern: {
       y: -6,
       squares: [
@@ -53,7 +49,7 @@ const resources = [
       es: 'Brinda la posibilidad de ejecutar reembolsos totales o parciales directamente desde el panel de manera segura y trazable.',
       en: 'Perform full or partial refunds directly from the panel in a secure way.',
     },
-    icon: PeronalizationTwoToneIcon,
+    icon: () => <BackSquare variant="TwoTone"/>,
     pattern: {
       y: 32,
       squares: [
@@ -68,7 +64,7 @@ const resources = [
       es: 'Brinda información precisa para resolver solicitudes rápidamente.',
       en: 'Provides precise information to resolve user inquiries efficiently.',
     },
-    icon: TransactionTwoToneIcon,
+    icon: () => <UserSearch variant="TwoTone"/>,
     pattern: {
       y: 22,
       squares: [[0, 1]],
@@ -80,7 +76,7 @@ const resources = [
       es: 'Permite acceso seguro y controlado a operaciones sensibles.',
       en: 'Ensures controlled and secure access to sensitive operations.',
     },
-    icon: FlexibilityTwoToneIcon,
+    icon: () => <SecuritySafe variant="TwoTone"/>,
     pattern: {
       y: 22,
       squares: [[0, 1]],
@@ -92,7 +88,7 @@ const resources = [
       es: 'Centraliza la gestión de pagos en una única herramienta.',
       en: 'Centralizes all payment management in a single platform.',
     },
-    icon: ErrorTwoToneIcon,
+    icon: () => <DocumentText variant="TwoTone"/>,
     pattern: {
       y: 22,
       squares: [[0, 1]],
