767ef2db61160da0797793726ba388b09406706f
Eduardo Guzmán H. <eduardo.guzman@evertecinc.com>
2025-10-14 09:29:25 -0500

    fix: ensure trailingSlash is set to true in nextConfig

--- next.config.mjs
+++ next.config.mjs
@@ -18,6 +18,7 @@ const nextConfig = {
   images: {
     unoptimized: true
   },
+  trailingSlash: true,
   output: 'export',
   reactStrictMode: true,
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'mdx'],
